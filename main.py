5a6394e696d0d91a2fc30c84eef97021e3549f17
Stone <stone@midway.hu>
2019-04-29 13:31:04 +0200

    Added cache for daytime, and automatic downloading stations.txt

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 # Requirements:
 #   pip2.7 install metar
 
-# stations.txt: https://www.aviationweather.gov/docs/metar/stations.txt
-
 
 from metar import Metar
 import requests
@@ -12,6 +10,9 @@ from datetime import datetime, timedelta
 import csv
 import pprint
 import re
+import os
+import pickle
+
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -28,19 +29,14 @@ def dt_parse(t):
        ret += timedelta(hours=int(t[20:22]), minutes=int(t[23:]))
     return ret
 
+def get_coordinates(icao):
 
-airports_csv = open('airports.csv', 'rb')
-airports = csv.reader(airports_csv, delimiter=';')
+    if not os.path.isfile('stations.txt'):
+        r = requests.get('https://www.aviationweather.gov/docs/metar/stations.txt', proxies=proxy)
+        with open('stations.txt', 'wb') as f:
+            f.write(r.content)
 
-for airport in airports:
-
-    icao = airport[0]
-    weather_station = airport[1]
-    min_cloud_base = int(airport[2])
-    desired_wind_dir = int(airport[3])
-
-    daylight = "UNKNOWN"
-    stations =  open('stations.txt')
+    stations = open('stations.txt')
     for station in stations:
         m = re.search('.+' + weather_station, station)
 
@@ -51,26 +47,73 @@ for airport in airports:
             lon_deg = station[47:50]
             lon_min = station[51:53]
             lon_side = station[53]
-            lat = float(lat_deg) + float(lat_min)/60
+            lat = float(lat_deg) + float(lat_min) / 60
             if lat_side == 'S':
                 lat *= -1
             lon = float(lon_deg) + float(lon_min) / 60
             if lon_side == 'W':
                 lon *= -1
 
-            resp = requests.get('https://api.sunrise-sunset.org/json?lat=' + str(lat) + '&lng=' + str(lon) + '&formatted=0', proxies=proxy)
-            json = resp.json()
-            sunrise = dt_parse(json['results']['sunrise'])
-            sunset = dt_parse(json['results']['sunset'])
-
-            now = datetime.utcnow()
-
-            if sunrise < now and now < sunset:
-                daylight = "DAY"
-            else:
-                daylight = "NIGHT"
-
     stations.close()
+
+    return (lat, lon)
+
+
+def daylight(icao):
+
+    cache = {}
+
+    now = datetime.utcnow()
+
+    if os.path.isfile('daylight.cache'):
+        cache_file = open('daylight.cache', 'rb')
+        cache = pickle.load(cache_file)
+        cache_file.close()
+
+    if icao in cache and cache[icao]['date'] + timedelta(days=7) > now:
+
+        sunrise = cache[icao]['sunrise']
+        sunset = cache[icao]['sunset']
+
+    else:
+
+        (lat, lon) = get_coordinates(icao)
+
+        resp = requests.get('https://api.sunrise-sunset.org/json?lat=' + str(lat) + '&lng=' + str(lon) + '&formatted=0', proxies=proxy)
+        json = resp.json()
+
+        sunrise = dt_parse(json['results']['sunrise'])
+        sunset = dt_parse(json['results']['sunset'])
+
+        cache[icao] = { 'sunrise': sunrise, 'sunset': sunset, 'date': now }
+
+        cache_file = open("daylight.cache", "wb")
+        pickle.dump(cache, cache_file)
+        cache_file.close()
+
+    if sunrise < now and now < sunset:
+        return True
+    else:
+        return False
+
+
+airports_csv = open('airports.csv', 'rb')
+airports = csv.reader(airports_csv, delimiter=';')
+
+for airport in airports:
+
+    if len(airport) < 4:
+        continue
+
+    icao = airport[0]
+    weather_station = airport[1]
+    min_cloud_base = int(airport[2])
+    desired_wind_dir = int(airport[3])
+
+    daylight_ok = daylight(icao)
+    wind_dir_ok = False
+    wind_speed_ok = False
+    cloud_base_ok = False
 
     resp = requests.get('http://tgftp.nws.noaa.gov/data/observations/metar/stations/' + weather_station + '.TXT', proxies=proxy)
     metar = resp.text.splitlines()[1]
@@ -80,22 +123,35 @@ for airport in airports:
     if len(decoded_metar.sky) > 0:
         if decoded_metar.sky[0][1] is not None:
             cloud_base = int(decoded_metar.sky[0][1].value())
+            if cloud_base > min_cloud_base:
+                cloud_base_ok = True
 
     wind_dir = 0
     if decoded_metar.wind_dir is not None:
         wind_dir = int(decoded_metar.wind_dir.value())
+        if desired_wind_dir + 360 - 45 < wind_dir + 360 and desired_wind_dir + 360 + 45 > wind_dir + 360:
+            wind_dir_ok = True
 
     wind_speed = int(decoded_metar.wind_speed.value())
+    if wind_speed > 10:
+        wind_speed_ok = True
 
-    if cloud_base >  min_cloud_base and (daylight == "DAY" or daylight == "UNKNOWN"):
-        if desired_wind_dir + 360 - 45 < wind_dir + 360 and desired_wind_dir + 360 + 45 > wind_dir + 360:
-            if wind_speed > 10:
-                state = "OK   "
+    if cloud_base_ok and daylight_ok:
+        if wind_dir_ok:
+            if wind_speed_ok:
+                status = "OK   "
             else:
-                state = "WEAK "
+                status = "WEAK "
         else:
-            state = "SO-SO"
+            status = "SO-SO"
     else:
-        state = "BAD  "
+        status = "BAD  "
 
-    print icao + ": " + state + " (" + daylight + ", cloud base: " + str(cloud_base) + " / " + str(min_cloud_base) + ", wind: " + str(wind_speed) + "@" + str(wind_dir) + " / " + str(desired_wind_dir) + ", " + metar + ")"
+    output_string = icao + ": " + status + " ("
+    if daylight_ok:
+        output_string += "day"
+    else:
+        output_string += "night/unknown"
+    output_string += ", cloud base: " + str(cloud_base) + " / " + str(min_cloud_base) + ", wind: " + str(wind_speed) + "@" + str(wind_dir) + " / " + str(desired_wind_dir) + ", " + metar + ")"
+
+    print output_string
